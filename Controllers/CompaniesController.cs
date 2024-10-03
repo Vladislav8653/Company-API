281e3a2f4a356ff16b52fc235935737bbc05c0b5
Vladislav8653 <arefin.vlad@gmail.com>
2024-10-03 13:26:08 +0300

    Added controller for GET that returns all companies by constructor injection of LoggerManager and RepositoryManager

---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,33 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers;
+
+[ApiController]
+[Route("api/[companies]")]
+public class CompaniesController : ControllerBase
+{
+    private IRepositoryManager _repository;
+    private ILoggerManager _logger;
+    
+    public CompaniesController(IRepositoryManager repository, ILoggerManager loggerManager)
+    {
+        _repository = repository;
+        _logger = loggerManager;
+    }
+
+    [HttpGet]
+    public IActionResult GetCompanies()
+    {
+        try
+        {
+            var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+            return Ok(companies);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Something went wrong in {nameof(GetCompanies)} action {ex}");
+            return StatusCode(500, "Internal server error.");
+        } 
+    }
+}
