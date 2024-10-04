1aeb8e7e8ff3ee1dd26762d1a77fa111b4881b2a
Vladislav8653 <arefin.vlad@gmail.com>
2024-10-04 21:07:03 +0300

    Implemented a custom formatter

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -31,4 +31,6 @@ public static class ServiceExtensions
     public static void ConfigureRepositoryManager(this IServiceCollection services) =>
         services.AddScoped<IRepositoryManager, RepositoryManager>();
 
+    public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+        builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
 }
--- Program.cs
+++ Program.cs
@@ -3,11 +3,18 @@ using LoggerService;
 using Microsoft.AspNetCore.HttpOverrides;
 using CompanyEmployees;
 using NLog;
+using Npgsql.Replication.PgOutput.Messages;
 
 LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters()
+.AddCustomCSVFormatter();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.AddControllers();
 builder.Services.ConfigureCors();
